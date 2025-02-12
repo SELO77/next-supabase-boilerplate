54e18ecba986d2c806ccae4898d122e84a0c7c81
sero <rochan87@gmail.com>
2025-02-12 18:55:59 +0900

    Add cursor rules

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
+import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
+
 export default function Home() {
-  
+
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <>
+      <div className="grid w-full gap-1.5">
+        <Label htmlFor="message">Korean</Label>
+        <Textarea placeholder="Type your message here." id="message" />
+      </div>
+    </>
   );
 }
